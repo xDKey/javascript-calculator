555a9a41a80ed45bd1abee168007a318b17ab2c0
Vladimir Mironov <DKey656@gmail.com>
2021-03-29 10:42:00 +0300

    refactoring end-to-end tests

--- cypress/integration/end-to-end-tests.spec.js
+++ cypress/integration/end-to-end-tests.spec.js
@@ -1,5 +1,8 @@
 const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const operatorButtons = ['/', 'x', '-', '+', 'AC', '=', '.'];
+before(() => {
+    cy.visit('/')
+})
 
 describe('Correctly rendering', () => {
   it('Contain div.calculator', () => {
