5895f140c14c35849bd411fc369d8f195b815baf
Vladimir Mironov <DKey656@gmail.com>
2021-03-29 14:28:39 +0300

    update some tests

--- src/first_unit_test.spec.js
+++
@@ -1,9 +0,0 @@
-import {mount} from 'cypress-react-unit-test'
-import Display from './component/Display/Display'
-
-describe('Display component', () => {
-    it('Works!', () => {
-        mount(<Display currentDisplay='test' accumulateDisplay='test'/>)
-        cy.contains('test').should('be.visible')
-    })
-})
