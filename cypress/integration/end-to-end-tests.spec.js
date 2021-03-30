d09cf1ddb83684e905e55b2e6361f0401afc72bf
Vladimir Mironov <DKey656@gmail.com>
2021-03-30 14:41:59 +0300

    fixed bug with cypress-react-selector library

--- cypress/integration/end-to-end-tests.spec.js
+++ cypress/integration/end-to-end-tests.spec.js
@@ -3,7 +3,7 @@ const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const operatorButtons = ['/', 'x', '-', '+', 'AC', '=', '.'];
 before(() => {
   cy.visit('/');
-  cy.waitForReact()
+  cy.waitForReact(1000, '#root')
 });
 
 describe('Correctly rendering', () => {
